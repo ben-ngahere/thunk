6cc653c900dc2c60bc19953cae030d50c071781e
Ben Ngahere <ngahereben@gmail.com>
2025-06-02 21:57:34 +1200

    landing page UI in progress

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -3,7 +3,6 @@ import { Outlet } from 'react-router'
 function App() {
   return (
     <div>
-      <h1>App Header</h1>
       <Outlet />
     </div>
   )
--- client/components/Home.tsx
+++ client/components/Home.tsx
@@ -1,29 +1,31 @@
-import { useState } from 'react'
-import { getGreeting } from '../apiClient.ts'
-import { useQuery } from '@tanstack/react-query'
+// import { useState } from 'react' 
+// import { getGreeting } from '../apiClient.ts'
+// import { useQuery } from '@tanstack/react-query' 
 
 const Home = () => {
-  const [count, setCount] = useState(0)
-
-  const {
-    data: greeting,
-    isError,
-    isPending,
-  } = useQuery({ queryKey: ['greeting', count], queryFn: getGreeting })
-
-  if (isPending) return <p>Loading...</p>
 
   return (
-    <>
-      {count}
-      <h1 className="text-3xl font-bold underline">{greeting}</h1>
-      {isError && (
-        <p style={{ color: 'red' }}>
-          There was an error retrieving the greeting.
-        </p>
-      )}
-      <button onClick={() => setCount(count + 1)}>Click</button>
-    </>
+  
+    <section className="hero is-fullheight is-warning">
+      <div className="hero-body">
+        <div className="container has-text-centered">
+          
+          {/* App Heading */}
+          <h1 className="title is-1 has-text-white mb-6">thunk</h1>
+          <div className="box has-background-white-ter py-5 px-6" style={{ opacity: 0.85, maxWidth: '400px', margin: 'auto' }}>
+            <h2 className="title is-5 has-text-grey-dark">welcome</h2>
+
+            {/* Login/Register Buttons */}
+            <div className="field">
+              <button className="button is-danger is-fullwidth mb-3">Login</button>
+            </div>
+            <div className="field">
+              <button className="button is-primary is-fullwidth">Register</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
