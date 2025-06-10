9a083af1d912ff4d500b8bd6c115a829cbd51eb4
Ben Ngahere <ngahereben@gmail.com>
2025-06-11 10:56:34 +1200

    Auth0 Set

--- client/main.tsx
+++ client/main.tsx
@@ -2,6 +2,7 @@ import { createRoot } from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Auth0Provider } from '@auth0/auth0-react'
 
 //import App from './components/App'
 import Home from './components/Home'
@@ -14,7 +15,20 @@ const queryClient = new QueryClient()
 document.addEventListener('DOMContentLoaded', () => {
   createRoot(document.getElementById('app') as HTMLElement).render(
     <QueryClientProvider client={queryClient}>
+      
+      {/* Auth0 */}
+      <Auth0Provider
+        domain="whai-2025-ben.au.auth0.com"
+        clientId="FouvVR0bcnWyb7vzqLXqS3L8bFWvXhvj"
+        authorizationParams={{
+          redirect_uri: window.location.origin, 
+        }}
+        // Auth0 - Pop-Up
+        useRefreshTokens={true} // Silent Refresh
+        cacheLocation="localstorage" // Local for Persistence
+      >
       <BrowserRouter>
+
         {/* routes */}
         <Routes>
           <Route path="/" element={<Home />} />
@@ -23,6 +37,7 @@ document.addEventListener('DOMContentLoaded', () => {
           <Route path="/log" element={<Log />} />
         </Routes>
       </BrowserRouter>
+      </Auth0Provider>
       <ReactQueryDevtools />
     </QueryClientProvider>,
   )
