08c6f4cd4fd10804430c3506f7f5bfdc09129b6a
Ben Ngahere <ngahereben@gmail.com>
2025-06-24 09:42:58 +1200

    more testing

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-    'import.meta.env.VITE_API_BASE_URL': JSON.stringify(process.env.VITE_API_BASE_URL || '/api/v1'),
-  },
+  envPrefix: 'VITE_',
   server: {
     headers: {
       'Cross-Origin-Opener-Policy': 'same-origin',
