9593356830e609eb161f0875964d0b48387fccba
Ben Ngahere <ngahereben@gmail.com>
2025-06-23 15:58:04 +1200

    updates

--- client/apiClient.ts
+++ client/apiClient.ts
@@ -1,8 +1,13 @@
 import request from 'superagent'
 
-const rootURL = new URL(`/api/v1`, document.baseURI)
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || '/api/v1'
 
 export async function getGreeting() {
-  const res = await request.get(`${rootURL}/greeting`)
+  const res = await request.get(`${API_BASE_URL}/greeting`)
   return res.body.greeting as string
+}
+
+export async function getMovies() {
+  const res = await request.get(`${API_BASE_URL}/movies`)
+  return res.body.movies
 }
