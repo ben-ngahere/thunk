1f05017d394b655085f6fec82544339f82c5fc04
Ben Ngahere <ngahereben@gmail.com>
2025-06-24 09:32:30 +1200

    testing backend

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-console.log('VITE_API_BASE_URL during Vite build:', process.env.VITE_API_BASE_URL)
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    'import.meta.env.VITE_API_BASE_URL': JSON.stringify(process.env.VITE_API_BASE_URL || '/api/v1'),
+  },
   server: {
     headers: {
       'Cross-Origin-Opener-Policy': 'same-origin',
