235de0fdb3252a7bf4d4ed6c8cfa3317b8dfb78e
Ben Ngahere <ngahereben@gmail.com>
2025-06-24 12:04:39 +1200

    more debugs

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-console.log('Vite Config Env Var Check:', process.env.VITE_API_BASE_URL);
-
 export default defineConfig({
   plugins: [react()],
   define: {
-    'import.meta.env': {
-      VITE_API_BASE_URL: JSON.stringify(process.env.VITE_API_BASE_URL)
-    }
+    '__API_BASE_URL__': JSON.stringify(process.env.VITE_API_BASE_URL),
   },
   server: {
     headers: {
