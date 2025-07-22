ea718e0f46b35d83a1756215a8d373189c82bdf1
Ben Ngahere <ngahereben@gmail.com>
2025-07-22 15:01:37 +1200

    knexfile fix

--- knexfile.js
+++ knexfile.js
@@ -1,4 +1,8 @@
 import * as Path from 'node:path'
+import { fileURLToPath } from 'node:url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = Path.dirname(__filename)
 
 export default {
   development: {
@@ -16,23 +20,24 @@ export default {
   },
 
   production: {
-  client: 'postgresql',
-  connection: {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT || 5432,
-    database: process.env.DB_NAME,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    ssl: { rejectUnauthorized: false }
-  },
-   migrations: {
+    client: 'postgresql',
+    connection: {
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT || 5432,
+      database: process.env.DB_NAME,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      ssl: { rejectUnauthorized: false }
+    },
+    migrations: {
       directory: Path.join(__dirname, 'server', 'db', 'migrations'), 
     },
     seeds: {
-      directory: Path.join(__dirname, 'server', 'db', 'seeds'),
+      directory: Path.join(__dirname, 'server', 'db', 'seeds'), 
     },
-  pool: {
-    min: 2,
-    max: 10,
-  },
-}}
+    pool: {
+      min: 2,
+      max: 10,
+    },
+  }
+}
